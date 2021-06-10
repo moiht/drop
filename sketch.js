110d8ae352c9cade2bc1afb39e2196420f4652c7
moiht <71550278+moiht@users.noreply.github.com>
2021-06-10 19:52:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -26,16 +26,24 @@ function setup() {
 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
-
-    helicopterSprite.velocityX=-1
+    box1Sprite = createSprite(width/2,640,100,20);
+	box1Sprite.shapeColor="red";
+	box2Sprite = createSprite(350,620,20,40);
+	box2Sprite.shapeColor="red";
+	box3Sprite = createSprite(450,620,20,40);
+	box3Sprite.shapeColor="red";
+	
+	helicopterSprite.velocityX=-1
 	
 	engine = Engine.create();
 	world = engine.world;
 
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
 	World.add(world, packageBody);
-	
-
+	box1 = Bodies.rectangle(width/2, 640, 100, 20 , {isStatic:true} );
+	box2 = Bodies.rectangle(350, 620, 20, 40 , {isStatic:true} );
+	box3 = Bodies.rectangle(450, 620, 20, 40 , {isStatic:true} );
+	World.add(world, box1);
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
@@ -59,7 +67,7 @@ function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
 	Matter.Body.setStatic( packageBody , false);
-    
+    packageBody.velocityX = -1;
   }
 }
 
